78cc0346f4ae1490a0d1ced39e79ceb8027f1342
JoshQuaintance <hasianjoshua@gmail.com>
2025-07-08 21:13:35 -0400

    Update logging for ci/cd and edited startscript for container to log all output of the script

--- cicd/redeploy.sh
+++ cicd/redeploy.sh
@@ -6,6 +6,23 @@ sudo singularity instance stop prod-bot
 cd esports-bot
 git pull
 
+# If the current git commit has the string "nodeenv.def" in it,
+# That means we need to rebuild the container because the 
+# definition file has been changed
+if git show --name-status HEAD | grep -q "nodeenv.def"; then
+    echo "Definition file changed, rebuilding container..."
+
+    # Backup the old container
+    mv env.sif env.sif.prev
+
+    # Build the new container
+    sudo singularity build env.sif nodenv.def
+
+    echo "Container rebuilt"
+fi
+
 # then start a new one that has a start script
 # that starts the bot
 sudo singularity instance start env.sif prod-bot
+
+
