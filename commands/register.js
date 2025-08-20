29e45c30ebc1b6ad164757498e833eec49ef9881
JoshQuaintance <hasianjoshua@gmail.com>
2025-08-19 21:11:23 -0400

    Add R6 to register command

--- commands/register.js
+++ commands/register.js
@@ -1,6 +1,5 @@
 import { MessageFlags, SlashCommandBuilder } from 'discord.js';
 import logger from '../utils/loggers/logger.js';
-import accessToken from '../accessToken.js';
 
 /**
  * @type {import('../typedefs.js').Command}
@@ -16,21 +15,25 @@ export default {
                 .setRequired(true)
                 .addChoices(
                     { name: 'Valorant', value: 'valorant' },
-                    { name: 'League of Legends', value: 'league' }
+                    { name: 'League of Legends', value: 'league' },
+                    { name: 'Rainbow Six Siege', value: 'r6' }
                 )
         ),
     async execute(interaction) {
         await interaction.deferReply({ flags: MessageFlags.Ephemeral });
 
         const game = interaction.options.getString('game');
-        logger.info(`User ${interaction.user.tag} registering for ${game}.`);
+        logger.info(`User <@${interaction.user.id}>(${interaction.user.tag}) registering for \`${game}\`.`);
         logger.info('Setting up prefilled form request...');
 
         const formUrls = {
             valorant:
                 'https://docs.google.com/forms/d/e/1FAIpQLSdEuJtc1s5JUx4maggG3MVAPJoJKb-i9zeJ3-93XcyffpbYjw/viewform?usp=pp_url&entry.903914901=',
-            league: 'https://docs.google.com/forms/d/e/1FAIpQLSekh7zYekNCQ1D-pEjsHN8OFTbOZDcARX5QeamKk53x9s7rjw/viewform?usp=pp_url&entry.1495248989='
+            league: 'https://docs.google.com/forms/d/e/1FAIpQLSekh7zYekNCQ1D-pEjsHN8OFTbOZDcARX5QeamKk53x9s7rjw/viewform?usp=pp_url&entry.1495248989=',
+            r6: 'https://docs.google.com/forms/d/e/1FAIpQLScfq1SnhKnQQp-Or9tW_rN4_KtAGOTIXZ_SQk7rRo2og1oDOg/viewform?usp=pp_url&entry.906228833='
         };
+
+        logger.info('Link sent successfully!');
 
         await interaction.editReply({
             content: `Please fill out the form for ${game}: ${formUrls[game]}${encodeURIComponent(interaction.user.tag)}`,
